e66bf3cd8cb18ad89e5a5ddf39dd47acabdeb02c
Thomas <ourliac.du.82@gmail.com>
2026-07-12 16:40:57 +0200

    api url for dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,6 @@ import { AppEnvironment } from './environment.model';
 export const environment: AppEnvironment = {
   name: 'DEV',
   production: false,
-  apiUrl: 'https://api-dev.example.com/v1',
+  apiUrl: 'https://fitness-back-8n5l.onrender.com/v1',
   enableDebugLogs: true
 };
